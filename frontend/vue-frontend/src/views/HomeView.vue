1f7226c7da4bdb81e055c0894666108e12856c0b
Ezinne Anne Emilia <onwukaezinneanne@gmail.com>
2024-12-24 06:36:46 +0100

    first commit

--- frontend/vue-frontend/src/views/HomeView.vue
+++ frontend/vue-frontend/src/views/HomeView.vue
@@ -1,9 +1,11 @@
 <script setup>
 import Header from '../components/Header.vue';
+import Features from '../components/Features.vue';
 </script>
 
 <template>
   <main>
     <Header />
+    <Features />
   </main>
 </template>
