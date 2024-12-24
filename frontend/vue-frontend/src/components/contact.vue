7f9a065e71688ac104112719d606be726b28c32a
Ezinne Anne Emilia <onwukaezinneanne@gmail.com>
2024-12-24 12:27:02 +0100

    first commit

---
+++ frontend/vue-frontend/src/components/contact.vue
@@ -0,0 +1,27 @@
+<template>
+    <header class="relative h-screen flex flex-col justify-between overflow-hidden">
+        <h1 class="text-4xl font-extrabold text-green-700 mb-8 text-center p-4">
+          Get In Touch
+        </h1>
+        <div class="flex flex-col md:flex-row items-center justify-center h-full">
+        <div class="md:w-1/2 p-4 text-center md:text-left">
+          <h2 class="text-2xl font-bold mb-4 text-green-600">Our Story</h2>
+          <p class="text-gray-700 leading-relaxed">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+          </p>
+          <p class="text-gray-700 leading-relaxed mt-4">
+            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
+          </p>
+        </div>
+        <div class="md:w-1/2 p-4">
+          <img src="../assets/farmers.jpg" alt="FarmEye Image" class="max-w-full h-auto rounded-lg shadow-lg">
+        </div>
+       </div>
+    </header>
+</template>
+
+<script>
+  export default {
+    name: "Contact",
+  };
+  </script>
