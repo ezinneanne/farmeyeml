4a7896a3252ece15f4839f1b1390ad2e8b80edc2
Ezinne Anne Emilia <onwukaezinneanne@gmail.com>
2024-12-25 16:15:19 +0100

    first commit

--- frontend/vue-frontend/src/components/about.vue
+++ frontend/vue-frontend/src/components/about.vue
@@ -1,12 +1,9 @@
 <template>
-    <section class="relative h-screen flex flex-col justify-between overflow-hidden">
+    <section class="relative h-screen flex flex-col justify-between overflow-hidden container mx-auto px-4">
         <h1 class="text-4xl font-extrabold text-green-700 mb-8 text-center p-4">
           About FarmEye
         </h1>
         <div class="flex flex-col md:flex-row items-center justify-center h-full">
-        <div class="md:w-1/2 p-4">
-          <img src="../assets/farmers.jpg" alt="FarmEye Image" class="max-w-full h-auto rounded-lg shadow-lg">
-        </div>
         <div class="md:w-1/2 p-4 text-center md:text-left">
           <h2 class="text-2xl font-bold mb-4 text-green-600">Our Story</h2>
           <p class="text-gray-700 leading-relaxed">
@@ -15,6 +12,9 @@
           <p class="text-gray-700 leading-relaxed mt-4">
             Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt.
           </p>
+        </div>
+        <div class="md:w-1/2 p-4">
+          <img src="../assets/farmers.jpg" alt="FarmEye Image" class="max-w-full h-auto rounded-lg shadow-lg">
         </div>
        </div>
       </section>
