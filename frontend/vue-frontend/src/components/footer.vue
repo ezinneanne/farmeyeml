3964a3aa64bd4f03b6531b452db89399f0fead8d
Ezinne Anne Emilia <onwukaezinneanne@gmail.com>
2024-12-25 15:24:39 +0100

    first commit

--- frontend/vue-frontend/src/components/footer.vue
+++ frontend/vue-frontend/src/components/footer.vue
@@ -1,59 +1,58 @@
 <template>
-    <footer class="bg-black text-white py-8">
+    <footer class="text-green-700 py-8">
       <section class="container mx-auto px-4">
-        <div class="flex flex-wrap justify-between gap-14">
+        <div class="flex flex-wrap justify-between">
           <!-- Logo and About -->
           <div class="w-full md:w-1/3">
-            <h1 class="text-2xl font-bold text-white mb-4">EZ Naturals.</h1>
-            <p class="text-gray-400">
-              EZ Naturals is a premier full-service beauty salon for women,
-              conveniently situated in the heart of Lagos, Nigeria... 
-              <a href="about.html" class="text-blue-500 font-semibold hover:text-white">Read More</a>
+            <h1 class="text-4xl font-bold text-green-700 mb-4">FarmEye</h1>
+            <p class="text-green-600">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. 
+                Nesciunt reiciendis maiores dignissimos iste 
+                optio officiis exercitationem quaerat velit alias.
             </p>
           </div>
   
           <!-- Info Links -->
           <div class="w-full md:w-1/3">
-            <h4 class="text-lg font-semibold mb-4">Info</h4>
+            <h4 class="text-lg font-bold mb-4">Info</h4>
             <ul class="space-y-2">
-              <li><a href="index.html" class="hover:text-gray-400">Home</a></li>
-              <li><a href="about.html" class="hover:text-gray-400">About</a></li>
-              <li><a href="services.html" class="hover:text-gray-400">Services</a></li>
-              <li><a href="lookbook.html" class="hover:text-gray-400">Lookbook</a></li>
-              <li><a href="contact.html" class="hover:text-gray-400">Contact Us <i class="far fa-envelope"></i></a></li>
+              <li><a href="#" class="hover:text-green-400">Home</a></li>
+              <li><a href="#" class="hover:text-green-400">Features</a></li>
+              <li><a href="#" class="hover:text-green-400">About</a></li>
+              <li><a href="#" class="hover:text-green-400">Contact Us <i class="far fa-envelope"></i></a></li>
             </ul>
           </div>
   
           <!-- Newsletter -->
           <div class="w-full md:w-1/3">
             <h4 class="text-lg font-semibold mb-4">Newsletter</h4>
-            <p class="text-gray-400 mb-4">
-              Subscribe to our newsletter for a weekly dose of news, updates, healthy hair tips, and exclusive discounts.
+            <p class="text-green-600 mb-4">
+              Subscribe to our newsletter for a weekly dose of news, updates, important farming tips, and exclusive access to our advanced tech products.
             </p>
             <form class="flex gap-2">
               <input
                 type="text"
                 placeholder="Your email"
                 required
-                class="w-full px-4 py-2 border border-gray-400 rounded-md bg-transparent text-white placeholder-gray-400 focus:outline-none focus:ring focus:ring-gray-500"
+                class="w-full px-4 py-2 border border-green-400 rounded-md bg-transparent text-white placeholder-green-400 focus:outline-none focus:ring focus:ring-green-500"
               />
               <button
                 type="submit"
-                class="bg-white text-black px-4 py-2 rounded-md font-medium hover:bg-gray-400"
+                class="bg-green-700 text-white px-4 py-2 rounded-md font-medium hover:bg-green-400"
               >
                 <a href="https://medium.com/@ezinneanne/subscribe">SUBSCRIBE</a>
               </button>
             </form>
             <div class="flex gap-4 mt-6">
-              <a href="https://github.com/ezinneanne" class="hover:text-gray-400"><i class="fab fa-facebook fa-2x"></i></a>
-              <a href="https://twitter.com/ezinne_anne" class="hover:text-gray-400"><i class="fab fa-instagram fa-2x"></i></a>
-              <a href="https://twitter.com/ezinne_anne" class="hover:text-gray-400"><i class="fab fa-twitter fa-2x"></i></a>
+              <a href="https://github.com/ezinneanne" class="hover:text-green-400"><i class="fab fa-facebook fa-2x"></i></a>
+              <a href="https://twitter.com/ezinne_anne" class="hover:text-green-400"><i class="fab fa-instagram fa-2x"></i></a>
+              <a href="https://twitter.com/ezinne_anne" class="hover:text-green-400"><i class="fab fa-twitter fa-2x"></i></a>
             </div>
           </div>
         </div>
       </section>
-      <hr class="border-gray-700 my-4" />
-      <p class="text-center text-gray-500">Copyright &copy; 2024. All rights reserved</p>
+      <hr class="border-green-700 my-4" />
+      <p class="text-center text-green-600">Copyright &copy; 2024. All rights reserved</p>
     </footer>
   </template>
   
